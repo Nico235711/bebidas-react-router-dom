2f30de57afd32f67f32a327a2a7b589ff5f9ab50
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-05 19:25:24 -0300

    Creando el Formulario de Búsqueda

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
-import { Link } from "react-router-dom"
+import { useMemo } from "react"
+import { NavLink, useLocation } from "react-router-dom"
 
 const Header = () => {
+
+  const { pathname } = useLocation()
+  const isHome = useMemo(() => pathname === "/", [pathname])
 
   return (
     <header className="bg-slate-800">
@@ -10,16 +14,52 @@ const Header = () => {
             <img src="/logo.svg" alt="logotipo" />
           </div>
           <nav className="flex gap-5">
-            <Link
+            <NavLink
               to="/"
-              className="text-white text-xl bg-orange-500 px-4 py-1 rounded-lg hover:bg-orange-600 transition-all"
-            >Inicio</Link>
-            <Link
+              className={({ isActive }) => 
+                isActive ? "text-orange-600 text-xl" : "text-white text-xl"}
+            >Inicio</NavLink>
+            <NavLink
               to="/favoritos"
-              className="text-white text-xl bg-orange-500 px-4 py-1 rounded-lg hover:bg-orange-600 transition-all"
-            >Favoritos</Link>
+              className={({ isActive }) => 
+                isActive ? "text-orange-600 text-xl" : "text-white text-xl"}
+            >Favoritos</NavLink>
           </nav>
         </div>
+
+        {isHome && (
+          <form className="my-5 md:w-1/2 2xl:1/3 bg-orange-400 rounded-lg p-10 shadow space-y-4">
+            <div>
+              <label
+                htmlFor="ingredient"
+                className="block text-white"
+              >Nombre de la Bebida o Ingredientes</label>
+              <input
+                type="text"
+                name="ingredient"
+                id="ingredient" 
+                className="mt-3 p-1 rounded-lg outline-none w-full"
+                placeholder="Nombre o Ingrediente. Ej: Vodka, Tequila, Café"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="category"
+                className="block text-white"
+              >Categoría</label>
+              <select name="category" id="category" className="mt-3 p-1 rounded-lg outline-none w-full">
+                <option value="">-- Seleccione una Categoría --</option>
+              </select>
+            </div>
+
+            <input
+              type="submit"
+              value="Buscar Bebida" 
+              className="mt-3 p-1 rounded-lg bg-orange-600 w-60 text-xl text-white cursor-pointer hover:bg-orange-700 transition-all"
+            />
+          </form>
+        )}
       </div>
     </header>
   )
