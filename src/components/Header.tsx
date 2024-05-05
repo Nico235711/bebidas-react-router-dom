243b8d0580492b812a800e99ed1d5e12a741ca7e
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-05 18:56:14 -0300

    Agregando Navegación entre páginas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
+import { Link } from "react-router-dom"
 
 const Header = () => {
 
   return (
     <header className="bg-slate-800">
-      <div className="mx-auto px-5 py-16">
+      <div className="mx-auto px-5 py-16 container">
         <div className="flex justify-between items-center">
           <div className="w-32">
             <img src="/logo.svg" alt="logotipo" />
           </div>
+          <nav className="flex gap-5">
+            <Link
+              to="/"
+              className="text-white text-xl bg-orange-500 px-4 py-1 rounded-lg hover:bg-orange-600 transition-all"
+            >Inicio</Link>
+            <Link
+              to="/favoritos"
+              className="text-white text-xl bg-orange-500 px-4 py-1 rounded-lg hover:bg-orange-600 transition-all"
+            >Favoritos</Link>
+          </nav>
         </div>
-        <nav></nav>
       </div>
     </header>
   )
