f86ce48f2afbe9aa829783a87f9940a229b70b32
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-05 22:34:22 -0300

    Obteniendo las Categorías

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,12 +1,17 @@
 import { StateCreator } from "zustand"
+import { getCategories } from "../apis/RecipeApi"
 
 type Category = {}
 
 export type RecipesSliceType = {
-  categories: Category[]
+  categories: Category[],
+  fetchCategories: () => Promise<void>
 }
 
 // importo StateCreator para tener autocompletado
 export const createRecipeSlice: StateCreator<RecipesSliceType> = () => ({
-  categories: []
+  categories: [],
+  fetchCategories: async () => {
+    await getCategories()
+  }
 })
