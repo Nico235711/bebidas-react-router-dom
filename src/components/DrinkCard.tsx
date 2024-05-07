64151da71ee1923b22ce123e3c7268cc74ea6071
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-07 14:02:01 -0300

    Finalizando el Código para mostrar las bebidas

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -7,13 +7,19 @@ type DrinkCardProps = {
 const DrinkCard = ({ drink }: DrinkCardProps) => {
 
   return (
-    <div className="flex">
-      <div className="">
-        imagen bebida
+    <div className="shadow-lg border my-5">
+      <div className="overflow-hidden">
+        <img
+          src={drink.strDrinkThumb}
+          alt={`Imagen de ${drink.strDrink}`} 
+          className="hover:scale-105 transition-all"
+        />
       </div>
-      <div className="">
-        <p>Nombre de la Bebida: {drink.strDrink}</p>
+      <div className="p-5">
+        <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
       </div>
+
+      <button type="button" className="m-3 p-2 rounded-lg bg-orange-600 w-60 text-xl text-white cursor-pointer hover:bg-orange-700 transition-all">Leer Receta</button>
     </div>
   )
 }
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -11,11 +11,11 @@ const IndexPage = () => {
     <>
       <h1 className="text-6xl font-extrabold">Recetas</h1>
       {hasDrinks ? (
-        <>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           {drinks.drinks.map(drink => (
             <DrinkCard drink={drink} key={drink.idDrink} />
           ))}
-        </>
+        </div>
       ) : (
         <p className="my-10 text-center text-2xl">No hay resultados aún, utiliza el formulario para buscar recetas</p>
       )}
