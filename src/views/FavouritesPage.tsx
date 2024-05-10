5f5d48c2e3bf1002b75b2fae60f90abd6af2bd76
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-10 14:00:21 -0300

    Primeros pasos con la página de favoritos

--- src/views/FavouritesPage.tsx
+++ src/views/FavouritesPage.tsx
@@ -1,8 +1,27 @@
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
 
 const FavouritesPage = () => {
 
+  const { favourites } = useAppStore()
+  const hasFavourites = useMemo(() => favourites.length, [favourites])
+
   return (
-    <h1>FavouritesPage</h1>
+    <>
+      <h1 className="text-6xl font-extrabold">Favoritos</h1>
+      {hasFavourites ? (
+        <>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {favourites.map(drink => (
+              <DrinkCard key={drink.idDrink} drink={drink} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <p className="my-10 text-center text-2xl">Los favoritos se mostrarán aquí</p>
+      )}
+    </>
   )
 }
 
